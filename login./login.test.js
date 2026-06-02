3fcc2f5e5f2f9492869b284077dfcf974ee6eb2c
Tinbuiz <115179364+Tinz288@users.noreply.github.com>
2026-06-02 14:12:14 +0700

    Add tests for login function

---
+++ login./login.test.js
@@ -0,0 +1,19 @@
+const login = require("./login");
+
+describe("Login Function", () => {
+    test("Đăng nhập đúng", () => {
+        expect(login("admin", "123")).toBe(true);
+    });
+
+    test("Sai username", () => {
+        expect(login("user", "123")).toBe(false);
+    });
+
+    test("Sai password", () => {
+        expect(login("admin", "456")).toBe(false);
+    });
+
+    test("Sai cả username và password", () => {
+        expect(login("user", "456")).toBe(false);
+    });
+});
