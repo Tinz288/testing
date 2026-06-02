af4e66c20d0d0b694874e0f3b612240ee22b4c74
Tinbuiz <115179364+Tinz288@users.noreply.github.com>
2026-06-02 14:44:53 +0700

    Update login.js

--- login.js
+++ login.js
@@ -1,8 +1,22 @@
-function login(username, password) {
-    return username === "admin" && password === "1234";
+function checkLogin(username, password) {
+    return username === "admin" && password === "123";
 }
 
-// Export để Jest có thể test
-if (typeof module !== "undefined") {
-    module.exports = login;
+if (typeof document !== "undefined") {
+    document.getElementById("loginForm").addEventListener("submit", function (e) {
+        e.preventDefault();
+
+        const username = document.getElementById("username").value;
+        const password = document.getElementById("password").value;
+
+        const result = document.getElementById("result");
+
+        if (checkLogin(username, password)) {
+            result.textContent = "Đăng nhập thành công!";
+        } else {
+            result.textContent = "Sai tài khoản hoặc mật khẩu!";
+        }
+    });
 }
+
+module.exports = { checkLogin };
